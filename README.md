d986198a69fe64a4270b5972cbc761f09e2d422e
Mah <for.my.git.work@gmail.com>
2022-01-08 21:50:55 +0100

    First initialized

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Course project
+
+This project was created during a react course.
+
+* `yarn` (to install) 
+
+* Run json server to get data from the server: `json-server --watch db.json -d 2000 -p 3001`
+
+* `yarn start`
+
---
+++ src/redux/feedbacks.js
@@ -0,0 +1,22 @@
+import * as ActionTypes from './ActionTypes'; 
+
+
+export const Feedbacks = (state = {
+    errMess: null,
+    feedback: []
+}, action) => {
+    switch(action.type) {
+        case ActionTypes.ADD_FEEDBACK:
+            return {...state, isLoading: false, errMess: null, feedback: action.payload}; 
+        
+        case ActionTypes.FEEDBACK_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, feedback: []}; 
+
+        case ActionTypes.POST_FEEDBACK:
+            var feedback = action.payload;
+            return {...state, messages: state.feedback.concat(feedback)}; 
+        default: 
+            return state; 
+    }
+}
+
